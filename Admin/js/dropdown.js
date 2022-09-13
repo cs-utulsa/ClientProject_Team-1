fb867fef90b514e86084f272f03a70bdc2226904
Kameron Eaton <kwe8364@utulsa.edu>
2022-09-12 21:40:21 -0500

    Added Notification Bar

---
+++ Admin/js/dropdown.js
@@ -0,0 +1,14 @@
+var box = document.getElementById('box');
+var down = false;
+
+function toggleNotification(){
+    if(down){
+    box.style.height = '0px';
+    box.style.opacity = 0;
+    down = false;
+} else {
+    box.style.height = '540px';
+    box.style.opacity = 1;
+    down = true;
+    }
+}
